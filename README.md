be302ff6de52df6f838b108feb7a77a3a5e00203
salenju <salenju@163.com>
2020-03-02 23:42:15 +0800

    init: init

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Next-Demo
+# Next Demo
 Next.js 学习Demo
---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  tabWidth: 2,
+  useTabs: true,
+  semi: false,
+  singleQuote: true,
+  TrailingCooma: "none",
+  bracketSpacing: true,
+  bracketSpacing: true,
+  arrowParens: 'always',
+  }
