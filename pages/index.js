ae3ef4e8b8c764d76ef3c176d19b2c8fb7a06f6d
salenju <judc@bwoil.com>
2020-03-11 17:22:35 +0800

    update: 添加路由钩子事件

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,27 @@ import Link from 'next/link'
 import Router from 'next/router'
 
 const Home = () => {
+	// 路由钩子事件
+	// routeChangeStart-路由开始变化时
+	Router.events.on('routeChangeStart', (...args) => {
+		console.log('===>routeChangeStart-路由开始变化时:', args)
+	})
+
+	// routeChangeComplete-路由结束变化时
+	Router.events.on('routeChangeComplete', (...args) => {
+		console.log('===>routeChangeComplete-路由结束变化时:', args)
+	})
+
+	// beforeHistoryChange-浏览器history触发前
+	Router.events.on('beforeHistoryChange', (...args) => {
+		console.log('===>beforeHistoryChange-浏览器history触发前:', args)
+	})
+
+	// routerChangeError-跳转发生错误时
+	Router.events.on('routerChangeError', (...args) => {
+		console.log('===>routerChangeError-跳转发生错误时:', args)
+	})
+
 	return (
 		<div>
 			<p>Hello Next.js</p>
@@ -10,8 +31,8 @@ const Home = () => {
 					<a>Go to PageA</a>
 				</Link>
 			</div>
-      <div>
-				<Link href={{pathname:'/PageD',query:{name:'A'}}}>
+			<div>
+				<Link href={{ pathname: '/PageD', query: { name: 'A' } }}>
 					<a>Go to PageD</a>
 				</Link>
 			</div>
