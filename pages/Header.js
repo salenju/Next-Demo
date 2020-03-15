7c0ef40dc18be68f6ebc194151f3b31d173170ba
salenju <salenju@163.com>
2020-03-15 12:29:20 +0800

    update: Next.js使用css和Antd UI框架

--- pages/Header.js
+++ pages/Header.js
@@ -1,14 +1,16 @@
 import Head from 'next/head'
+import { Button } from 'antd'
 
 const Header = () => {
 	return (
 		<div>
-      {/* 在这里写页面的title 等信息 */}
+			{/* 在这里写页面的title 等信息 */}
 			<Head>
 				<title>Welcome to Salen Ju Blog</title>
 				<meta charSet="utf-8" />>
 			</Head>
 			Salen Ju
+			<Button type='primary'>赞</Button>
 		</div>
 	)
 }
